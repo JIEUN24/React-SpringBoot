d86c327f45e4af6d976fdcda4b47701c22f51ca5
JIEUN <pandaa0628@gmail.com>
2023-08-18 18:32:47 +0900

    REST API URI 수정

--- src/main/frontend/src/pages/CreatePostPage.jsx
+++ src/main/frontend/src/pages/CreatePostPage.jsx
@@ -1,160 +1,160 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {useNavigate, useParams} from "react-router-dom";
 import styled from "styled-components";
 import axios from "axios";
 import Swal from "sweetalert2";
 
 const CreatePostPage = () => {
-  const navigate = useNavigate();
-  const params = useParams();
-  const postId = params?.id;
-  const [post, setPost] = useState({
-    title: "",
-    content: "",
-    userName: "",
-  });
-  const [detail, setDetail] = useState({});
+    const navigate = useNavigate();
+    const params = useParams();
+    const postId = params?.id;
+    const [post, setPost] = useState({
+        title: "",
+        content: "",
+        userName: "",
+    });
+    const [detail, setDetail] = useState({});
 
-  const onChangeHandler = (e) => {
-    const { name, value } = e.target;
-    setPost({ ...post, [name]: value });
-  };
+    const onChangeHandler = (e) => {
+        const {name, value} = e.target;
+        setPost({...post, [name]: value});
+    };
 
-  const addContent = () => {
-    axios
-      .post("/board/add", {
-        title: post?.title,
-        content: post?.content,
-        userName: post?.userName,
-      })
-      .then((res) => {
-        Swal.fire({
-          position: "top-center",
-          icon: "success",
-          title: "등록이 완료되었습니다",
-          showConfirmButton: false,
-          timer: 1000,
-        });
-        navigate("/");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+    const addContent = () => {
+        axios
+            .post("/posts", {
+                title: post?.title,
+                content: post?.content,
+                userName: post?.userName,
+            })
+            .then((res) => {
+                Swal.fire({
+                    position: "top-center",
+                    icon: "success",
+                    title: "등록이 완료되었습니다",
+                    showConfirmButton: false,
+                    timer: 1000,
+                });
+                navigate("/");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
 
-  const upDateContent = (id) => {
-    axios
-      .put("/board/update", {
-        id: id,
-        title: post?.title,
-        content: post?.content,
-        userName: post?.userName,
-      })
-      .then((res) => {
-        Swal.fire({
-          position: "top-center",
-          icon: "success",
-          title: "수정이 완료되었습니다",
-          showConfirmButton: false,
-          timer: 1000,
-        });
-        navigate("/");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+    const upDateContent = (id) => {
+        axios
+            .patch("/posts", {
+                id: id,
+                title: post?.title,
+                content: post?.content,
+                userName: post?.userName,
+            })
+            .then((res) => {
+                Swal.fire({
+                    position: "top-center",
+                    icon: "success",
+                    title: "수정이 완료되었습니다",
+                    showConfirmButton: false,
+                    timer: 1000,
+                });
+                navigate("/");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
 
-  useEffect(() => {
-    if (postId) {
-      axios
-        .get(`/board/list/${postId}`)
-        .then((res) => {
-          setDetail(res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }, []);
+    useEffect(() => {
+        if (postId) {
+            axios
+                .get(`/posts/${postId}`)
+                .then((res) => {
+                    setDetail(res.data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    }, []);
 
-  return (
-    <CreatePostWrap>
-      <CreateHeader>
-        <h3>{postId ? "리뷰 수정" : "리뷰 작성"}</h3>
-        <div>
-          <button
-            onClick={() => {
-              Swal.fire({
-                title: "리뷰 작성을 취소하시겠습니까?",
-                text: "기존에 작성하던 글이 유지되지 않습니다.",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#3c52bb",
-                cancelButtonColor: "#d33",
-                confirmButtonText: "확인",
-                cancelButtonText: "취소",
-              }).then((result) => {
-                if (result.isConfirmed) {
-                  navigate("/");
-                } else {
-                  return;
-                }
-              });
-            }}
-          >
-            취소
-          </button>
-          <button
-            onClick={() => {
-              if (postId) {
-                upDateContent(postId);
-              } else {
-                addContent();
-              }
-            }}
-          >
-            {postId ? "저장" : "등록"}
-          </button>
-        </div>
-      </CreateHeader>
-      <CreateBody>
-        <div className='contentBox'>
-          <div>작성자</div>
-          <input
-            name='userName'
-            type='text'
-            defaultValue={postId ? detail.userName : post.userName}
-            onChange={(e) => {
-              onChangeHandler(e);
-            }}
-          />
-        </div>
-        <div className='contentBox'>
-          <div>제목</div>
-          <input
-            name='title'
-            type='text'
-            defaultValue={postId ? detail.title : post.title}
-            onChange={(e) => {
-              onChangeHandler(e);
-            }}
-          />
-        </div>
-        <div className='contentBox'>
-          <div>내용</div>
-          <textarea
-            name='content'
-            type='text'
-            defaultValue={postId ? detail.content : post.content}
-            onChange={(e) => {
-              onChangeHandler(e);
-            }}
-          />
-        </div>
-      </CreateBody>
-    </CreatePostWrap>
-  );
+    return (
+        <CreatePostWrap>
+            <CreateHeader>
+                <h3>{postId ? "리뷰 수정" : "리뷰 작성"}</h3>
+                <div>
+                    <button
+                        onClick={() => {
+                            Swal.fire({
+                                title: "리뷰 작성을 취소하시겠습니까?",
+                                text: "기존에 작성하던 글이 유지되지 않습니다.",
+                                icon: "warning",
+                                showCancelButton: true,
+                                confirmButtonColor: "#3c52bb",
+                                cancelButtonColor: "#d33",
+                                confirmButtonText: "확인",
+                                cancelButtonText: "취소",
+                            }).then((result) => {
+                                if (result.isConfirmed) {
+                                    navigate("/");
+                                } else {
+                                    return;
+                                }
+                            });
+                        }}
+                    >
+                        취소
+                    </button>
+                    <button
+                        onClick={() => {
+                            if (postId) {
+                                upDateContent(postId);
+                            } else {
+                                addContent();
+                            }
+                        }}
+                    >
+                        {postId ? "저장" : "등록"}
+                    </button>
+                </div>
+            </CreateHeader>
+            <CreateBody>
+                <div className='contentBox'>
+                    <div>작성자</div>
+                    <input
+                        name='userName'
+                        type='text'
+                        defaultValue={postId ? detail.userName : post.userName}
+                        onChange={(e) => {
+                            onChangeHandler(e);
+                        }}
+                    />
+                </div>
+                <div className='contentBox'>
+                    <div>제목</div>
+                    <input
+                        name='title'
+                        type='text'
+                        defaultValue={postId ? detail.title : post.title}
+                        onChange={(e) => {
+                            onChangeHandler(e);
+                        }}
+                    />
+                </div>
+                <div className='contentBox'>
+                    <div>내용</div>
+                    <textarea
+                        name='content'
+                        type='text'
+                        defaultValue={postId ? detail.content : post.content}
+                        onChange={(e) => {
+                            onChangeHandler(e);
+                        }}
+                    />
+                </div>
+            </CreateBody>
+        </CreatePostWrap>
+    );
 };
 
 const CreatePostWrap = styled.div`
